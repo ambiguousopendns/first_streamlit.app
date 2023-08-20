8b20296a15bb7f58659433372ad64f53ec790804
ambiguousopendns <142494861+ambiguousopendns@users.noreply.github.com>
2023-08-20 12:07:38 +1000

    Update streamlit_app.py
    allow user to add fruit to list

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,3 +42,8 @@ my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+#New section to allow user to add fruit to list
+add_my_fruit = streamlit.text_input('What fruit would you like information about?','')
+my_cur.execute("insert into fruit_load_list values ('" + add_my_fruit + "')")
+streamlit.text('Thanks for adding ' + add_my_fruit)
