5607caf2b07a20c01874ac513ed37364a092bfa8
ambiguousopendns <142494861+ambiguousopendns@users.noreply.github.com>
2023-08-20 12:43:29 +1000

    Update streamlit_app.py
    Add stop

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,6 +39,8 @@ try:
 except URLError as e:
   streamlit.error()
 
+streamlit.stop()
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list order by 1")
